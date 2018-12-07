fdae5828b8591f6cd55c6d2404abf49e1d63a87c
E-man <emmanuel.donohue@gmail.com>
2018-12-07 11:27:29 -0800

    csv reader works with abs path

---
+++ PyBank/main.py
@@ -0,0 +1,32 @@
+
+#allows us to create file paths across operating systems
+import os
+
+#module for reading csv file
+import csv
+######################################
+#encode path
+from pathlib import Path 
+
+#absolute path
+path = Path('/Users/emmanuelfabre/Desktop/03_python_homework_Instructions_PyBank_Resources_budget_data.csv')
+
+#read csv file
+with open(path, newline='') as csvfile:
+	# CSV reader specifies delimiter and variable that holds contents
+	csvreader = csv.reader(csvfile, delimiter=',')
+
+
+	print(csvreader)
+
+	# Read the header row first (skip this step if there is now header)
+	csv_header = next(csvreader)
+	print(f"CSV Header: {csv_header}")
+
+
+	#Add function to print out file and see if it prints out correctly
+	for row in csvreader:
+		print(row)
+		
+
+
