a924fdbeab27c7555fa854f9e254f32fb3a9ac4a
E-man <emmanuel.donohue@gmail.com>
2018-12-11 11:56:00 -0800

    add func to sum pnl

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,36 +13,39 @@ from pathlib import Path
 
 path = Path('/Users/emmanuelfabre/Desktop/03_python_homework_Instructions_PyBank_Resources_budget_data.csv')
 
-
-with open(path, newline='') as csvfile:
-	# CSV reader specifies delimiter and variable that holds contents
-	csvreader = csv.reader(csvfile, delimiter=',')
-
-	print(csvreader)
-
-		#Add function to print out file and see if it prints out correctly
-	for row in csvreader:
-		print(row)
-
-
-BudgetDataCsv = os.path.join('/Users/emmanuelfabre/Desktop/03_python_homework_Instructions_PyBank_Resources_budget_data.csv')
-
 #function gettotals
-def getTotals(BudgetCsv):
+def get_months(BudgetCsv):
 	with open(BudgetCsv, "r") as csvfile:
-		csvreader = csv.reader(csvfile, delimiter = ",")
+		csvreader = csv.reader(csvfile, delimiter = ',')
 		next(csvreader)
 		#make list out of rows
 		row_count = list(csvreader)	
 		#count rows in csv
 		months = len(row_count)
 	
-	#TotalNet = sum(int(BudgetDataCsv[1]))	#or budgetdatacsv, budgetdata?
-				#google sum(int) in csv col
-		print(months)
+		print("The total number of months included is: " + str(months))
 
 
-getTotals(BudgetDataCsv)
+
+def sum_pnl(BudgetCsv):
+	with open(BudgetCsv, "r") as csvfile:
+		csvreader = csv.reader(csvfile, delimiter = ',')
+		next(csvreader)
+
+		pnl_count = 0
+		for row in csvreader:
+			pnl_count += int(row[1])	#neg nums seen as string unless converted to int
+		print("The total net amount of Profit/Losses is: " + str(pnl_count))			
+									#prints out pnl col. If you indent it once more..
+									#it's in the for loop, and will print after each..
+									#addition. 
+
+
+
+get_months(path)
+sum_pnl(path)
+
+
 
 
 
